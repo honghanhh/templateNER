92aa789ec988861c800b603cc2edffaf0fa862a4
Hồng Hạnh <hanh.usth@gmail.com>
2023-02-01 13:35:00 +0100

    rm data files

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 python preprocess.py --input_dir './public_data/DE-German/' --output_dir './preprocessed_data/' --lang 'de'
 
 # Training
+python train.py --train "./preprocessed_data/de-train.csv" --test "./preprocessed_data/de-dev.csv" --output_dir "./ex_bart_de_de_t1_de"
 
 # Inference
